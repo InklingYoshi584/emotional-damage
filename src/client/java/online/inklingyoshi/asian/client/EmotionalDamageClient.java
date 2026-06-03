b50f0496935add6582b91591de5f5082a9886bc9
InklingYoshi584 <yoshi1024@163.com>
2026-06-03 21:43:23 +0800

    fix: register entity renderers for ThrownSlipper and ThrownItemProjectile (26.1 EntityRenderer API)

--- src/client/java/online/inklingyoshi/asian/client/EmotionalDamageClient.java
+++ src/client/java/online/inklingyoshi/asian/client/EmotionalDamageClient.java
@@ -1,11 +1,32 @@
 package online.inklingyoshi.asian.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.state.EntityRenderState;
+import net.minecraft.world.entity.Entity;
+import online.inklingyoshi.asian.attack.ModEntities;
 import online.inklingyoshi.asian.difficulty.ClientModDifficulty;
 
 public class EmotionalDamageClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ClientModDifficulty.registerReceiver();
+
+        EntityRendererRegistry.register(ModEntities.THROWN_SLIPPER, ctx ->
+            new EntityRenderer<Entity, EntityRenderState>(ctx) {
+                @Override
+                public EntityRenderState createRenderState() {
+                    return new EntityRenderState();
+                }
+            });
+
+        EntityRendererRegistry.register(ModEntities.THROWN_ITEM, ctx ->
+            new EntityRenderer<Entity, EntityRenderState>(ctx) {
+                @Override
+                public EntityRenderState createRenderState() {
+                    return new EntityRenderState();
+                }
+            });
     }
 }
